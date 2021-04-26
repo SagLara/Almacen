a5c376d4f2f36ced6283f984a09f081c67832f22
Sergio Gomez <laragsergio1803@gmail.com>
2021-04-26 15:09:01 -0500

    Priver avance conexion con base de datos mysql

---
+++ src/database.js
@@ -0,0 +1,25 @@
+const mysql = require('promise-mysql')
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'almacen_jis'
+})
+
+/* conn.connect(function(err) {
+    if (err) {
+        console.log('code :>> ', err.code);
+        console.log('fatal :>> ', err.fatal);
+    }
+}) */
+
+function getConnection() {
+    return conn;
+}
+
+module.exports = {
+    getConnection,
+    conn
+}
